6a3bdfce37128795152019de3ce838f258dbabbc
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-03-17 17:09:31 -0400

    initial commit

---
+++ app/src/main/java/com/example/sudhir_project_phase/Login_Form_Activity.java
@@ -0,0 +1,22 @@
+package com.example.sudhir_project_phase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login_Form_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__form_);
+        getSupportActionBar().setTitle("Login");
+    }
+
+    public void btn_signup_form(View view) {
+
+        startActivity(new Intent(getApplicationContext(),SignUp_Form_Activity.class));
+    }
+}
---
+++ app/src/main/java/com/example/sudhir_project_phase/SignUp_Form_Activity.java
@@ -0,0 +1,15 @@
+package com.example.sudhir_project_phase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUp_Form_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up__form_);
+        getSupportActionBar().setTitle("Sign UP");
+    }
+}
