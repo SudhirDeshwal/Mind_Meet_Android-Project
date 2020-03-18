c5f0cbf4c9fa861e43dd46b19a1b679b9dffc8e1
SudhirDeshwal <50604228+SudhirDeshwal@users.noreply.github.com>
2020-03-17 20:49:23 -0400

    commit_shivam_sudhir_test

---
+++ app/src/main/java/com/example/sudhir_project_phase/sudhir_shivam_test.java
@@ -0,0 +1,14 @@
+package com.example.sudhir_project_phase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sudhir_shivam_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sudhir_shivam_test);
+    }
+}
