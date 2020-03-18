94824e37262c7450b04e4b9d194e96ca1c844ada
Shivghai18 <42421365+Shivghai18@users.noreply.github.com>
2020-03-18 05:27:11 -0400

    Adding my files in the project

--- app/src/main/java/com/example/sudhir_project_phase/DetailsPage.java
+++ app/src/main/java/com/example/sudhir_project_phase/DetailsPage.java
@@ -2,13 +2,18 @@ package com.example.sudhir_project_phase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class test_shivam extends AppCompatActivity {
+
+public class DetailsPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test_shivam);
+
+        setContentView(R.layout.activity_details_page);
     }
+
 }
--- app/src/main/java/com/example/sudhir_project_phase/MainActivity.java
+++ app/src/main/java/com/example/sudhir_project_phase/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.sudhir_project_phase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+    }
+
+    public void detailsPage(View v)
+    {
+        Intent i=new Intent(this,DetailsPage.class);
+
+        startActivity(i);
+
+
+
     }
 }
