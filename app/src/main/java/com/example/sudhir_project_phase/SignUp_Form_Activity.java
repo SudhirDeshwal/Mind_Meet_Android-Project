da24a076daaeff18c61891cbdbae1364a4d6b2a8
connect-dhruvi <44392761+connect-dhruvi@users.noreply.github.com>
2020-04-08 23:23:51 -0400

    no message

--- app/src/main/java/com/example/sudhir_project_phase/SignUp_Form_Activity.java
+++ app/src/main/java/com/example/sudhir_project_phase/SignUp_Form_Activity.java
@@ -5,18 +5,87 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUp_Form_Activity extends AppCompatActivity {
+    private FirebaseDatabase firebasedatabase;
+    private DatabaseReference databasereference;
+    EditText et_username,et_email,et_phone,et_password,et_confirmpassword;
+    RadioGroup rg_gender;
+    RadioButton radioMale,radioFemale;
+
+    String gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up__form_);
         getSupportActionBar().setTitle("Sign UP");
+
+        firebasedatabase= FirebaseDatabase.getInstance();
+        databasereference=firebasedatabase.getReference().child("Users");
+
+        et_email=(EditText)findViewById(R.id.et_email);
+        et_username=(EditText)findViewById(R.id.et_userename);
+        et_phone=(EditText)findViewById(R.id.et_phone);
+        et_password=(EditText)findViewById(R.id.et_password);
+        et_confirmpassword=(EditText)findViewById(R.id.et_confirm_password);
+
+        rg_gender=(RadioGroup)findViewById(R.id.radio_gender);
+
+        radioMale=(RadioButton)findViewById(R.id.radio_male);
+        radioFemale=(RadioButton)findViewById(R.id.radio_female);
     }
 
     public void btn_details_1(View view) {
 
         startActivity(new Intent(getApplicationContext(),Details_Page_1.class));
+        if(et_phone.getText()==null){
+            Toast t =Toast.makeText(getApplicationContext(),"phone field cant be left empty",Toast.LENGTH_LONG);
+            t.show();
+            saveUser();
+        }
+    }
+
+    public void RadioClicked(View view) {
+
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_male:
+                if (checked)
+                    gender="male";
+                    break;
+            case R.id.radio_female:
+                if (checked)
+                    gender="female";
+                    break;
+        }
+    }
+
+    private void saveUser(){
+
+        String username=et_username.getText().toString();
+        String email=et_email.getText().toString();
+        String password =et_password.getText().toString();
+        String phone=et_phone.getText().toString();
+        User user=new User(email,username,password,phone,gender,"");
+        System.out.println(username+email+password+phone+username);
+        databasereference.push().setValue(user);
+    }
+    private void clean(){
+        et_email.setText("");
+        et_username.setText("");
+        et_confirmpassword.setText("");
+        et_password.setText("");
+        et_phone.setText("");
     }
 }
--- app/src/main/java/com/example/sudhir_project_phase/User.java
+++ app/src/main/java/com/example/sudhir_project_phase/User.java
@@ -1,4 +1,7 @@
+package com.example.sudhir_project_phase;
+
 public class User {
+    private  String imageURL;
     private String id;
     private String email;
     private String phone;
@@ -6,12 +9,65 @@ public class User {
     private String password;
     private String username;
 
-    public User(String id, String email, String phone, String gender, String password, String username) {
-        this.id = id;
+    public User( String email, String phone, String gender, String password, String username,String imageURL) {
+     //  this.id = id;
         this.email = email;
         this.phone = phone;
         this.gender = gender;
         this.password = password;
         this.username = username;
+        this.imageURL=imageURL;
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+
+
+
 }
